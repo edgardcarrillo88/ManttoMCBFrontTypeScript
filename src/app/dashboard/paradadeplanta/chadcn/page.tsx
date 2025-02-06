21a5b4db952bed6ef8af19bd0b1540874546c47f
Edgard Carrillo <ea_carrillo@hotmail.com>
2025-02-06 11:11:11 -0500

    1erCommit06Feb01

--- src/app/dashboard/paradadeplanta/chadcn/page.tsx
+++ src/app/dashboard/paradadeplanta/chadcn/page.tsx
@@ -230,133 +230,133 @@ type ChartLineMultipleProps = {
   LineaReal: TypeLineaBase;
 };
 
-export function ChartLine({ data }: ChartLineProps) {
-  return (
-    <>
-      <Card className="">
-        <CardHeader>
-          <CardTitle>Linea Base General</CardTitle>
-          <CardDescription>PdP Enero 2025</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ChartContainer config={chartConfig}>
-            <LineChart
-              accessibilityLayer
-              // data={chartData}
-              data={data}
-              margin={{
-                left: 12,
-                right: 12,
-              }}
-            >
-              <CartesianGrid vertical={false} />
-              <XAxis
-                // dataKey="month"
-                dataKey="Ejex"
-                tickLine={false}
-                axisLine={false}
-                tickMargin={8}
-                // tickFormatter={(value: any) => value.slice(0, 3)}
-                tickFormatter={(value) => {
-                  const date = new Date(value);
-                  return date.toLocaleDateString("en-US", {
-                    month: "short",
-                    day: "numeric",
-                  });
-                }}
-              />
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent hideLabel />}
-              />
-              <Line
-                // dataKey="desktop"
-                dataKey="hh cum"
-                type="monotone"
-                stroke="var(--color-desktop)"
-                strokeWidth={2}
-                dot={{
-                  fill: "var(--color-desktop)",
-                }}
-                activeDot={{
-                  r: 6,
-                }}
-              />
-              <ChartLegend
-                content={
-                  <ChartLegendContent
-                    //  nameKey="desktop"
-                    nameKey="hh cum"
-                  />
-                }
-              />
-            </LineChart>
-          </ChartContainer>
-        </CardContent>
-        <CardFooter className="flex-col items-start gap-2 text-sm">
-          <div className="flex gap-2 font-medium leading-none">
-            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-          </div>
-          <div className="leading-none text-muted-foreground">
-            Showing total visitors for the last 6 months
-          </div>
-        </CardFooter>
-      </Card>
+// export function ChartLine({ data }: ChartLineProps) {
+//   return (
+//     <>
+//       <Card className="">
+//         <CardHeader>
+//           <CardTitle>Linea Base General</CardTitle>
+//           <CardDescription>PdP Enero 2025</CardDescription>
+//         </CardHeader>
+//         <CardContent>
+//           <ChartContainer config={chartConfig}>
+//             <LineChart
+//               accessibilityLayer
+//               // data={chartData}
+//               data={data}
+//               margin={{
+//                 left: 12,
+//                 right: 12,
+//               }}
+//             >
+//               <CartesianGrid vertical={false} />
+//               <XAxis
+//                 // dataKey="month"
+//                 dataKey="Ejex"
+//                 tickLine={false}
+//                 axisLine={false}
+//                 tickMargin={8}
+//                 // tickFormatter={(value: any) => value.slice(0, 3)}
+//                 tickFormatter={(value) => {
+//                   const date = new Date(value);
+//                   return date.toLocaleDateString("en-US", {
+//                     month: "short",
+//                     day: "numeric",
+//                   });
+//                 }}
+//               />
+//               <ChartTooltip
+//                 cursor={false}
+//                 content={<ChartTooltipContent hideLabel />}
+//               />
+//               <Line
+//                 // dataKey="desktop"
+//                 dataKey="hh cum"
+//                 type="monotone"
+//                 stroke="var(--color-desktop)"
+//                 strokeWidth={2}
+//                 dot={{
+//                   fill: "var(--color-desktop)",
+//                 }}
+//                 activeDot={{
+//                   r: 6,
+//                 }}
+//               />
+//               <ChartLegend
+//                 content={
+//                   <ChartLegendContent
+//                     //  nameKey="desktop"
+//                     nameKey="hh cum"
+//                   />
+//                 }
+//               />
+//             </LineChart>
+//           </ChartContainer>
+//         </CardContent>
+//         <CardFooter className="flex-col items-start gap-2 text-sm">
+//           <div className="flex gap-2 font-medium leading-none">
+//             Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+//           </div>
+//           <div className="leading-none text-muted-foreground">
+//             Showing total visitors for the last 6 months
+//           </div>
+//         </CardFooter>
+//       </Card>
 
-      <Card className="">
-        <CardHeader>
-          <CardTitle>Line Chart - Dots</CardTitle>
-          <CardDescription>January - June 2024</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <ChartContainer config={chartConfig}>
-            <LineChart
-              accessibilityLayer
-              data={chartData}
-              margin={{
-                left: 12,
-                right: 12,
-              }}
-            >
-              <CartesianGrid vertical={false} />
-              <XAxis
-                dataKey="month"
-                tickLine={false}
-                axisLine={false}
-                tickMargin={8}
-                tickFormatter={(value: any) => value.slice(0, 3)}
-              />
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent hideLabel />}
-              />
-              <Line
-                dataKey="mobile"
-                type="natural"
-                stroke="var(--color-desktop)"
-                strokeWidth={2}
-                dot={{
-                  fill: "var(--color-desktop)",
-                }}
-                activeDot={{
-                  r: 6,
-                }}
-              />
-            </LineChart>
-          </ChartContainer>
-        </CardContent>
-        <CardFooter className="flex-col items-start gap-2 text-sm">
-          <div className="flex gap-2 font-medium leading-none">
-            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-          </div>
-          <div className="leading-none text-muted-foreground">
-            Showing total visitors for the last 6 months
-          </div>
-        </CardFooter>
-      </Card>
-    </>
-  );
-}
+//       <Card className="">
+//         <CardHeader>
+//           <CardTitle>Line Chart - Dots</CardTitle>
+//           <CardDescription>January - June 2024</CardDescription>
+//         </CardHeader>
+//         <CardContent>
+//           <ChartContainer config={chartConfig}>
+//             <LineChart
+//               accessibilityLayer
+//               data={chartData}
+//               margin={{
+//                 left: 12,
+//                 right: 12,
+//               }}
+//             >
+//               <CartesianGrid vertical={false} />
+//               <XAxis
+//                 dataKey="month"
+//                 tickLine={false}
+//                 axisLine={false}
+//                 tickMargin={8}
+//                 tickFormatter={(value: any) => value.slice(0, 3)}
+//               />
+//               <ChartTooltip
+//                 cursor={false}
+//                 content={<ChartTooltipContent hideLabel />}
+//               />
+//               <Line
+//                 dataKey="mobile"
+//                 type="natural"
+//                 stroke="var(--color-desktop)"
+//                 strokeWidth={2}
+//                 dot={{
+//                   fill: "var(--color-desktop)",
+//                 }}
+//                 activeDot={{
+//                   r: 6,
+//                 }}
+//               />
+//             </LineChart>
+//           </ChartContainer>
+//         </CardContent>
+//         <CardFooter className="flex-col items-start gap-2 text-sm">
+//           <div className="flex gap-2 font-medium leading-none">
+//             Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+//           </div>
+//           <div className="leading-none text-muted-foreground">
+//             Showing total visitors for the last 6 months
+//           </div>
+//         </CardFooter>
+//       </Card>
+//     </>
+//   );
+// }
 
 export function ChartLIneInteractive({
   LineaBase,
