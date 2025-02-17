c6d81a63bb2403b9771b9abad294b66623786cdd
Edgard Carrillo <ea_carrillo@hotmail.com>
2025-02-17 13:09:13 -0500

    Update17Feb03

--- src/app/dashboard/gestionmantto/general/page.tsx
+++ src/app/dashboard/gestionmantto/general/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <ProtectedRouteComponent password={process.env.NEXT_PUBLIC_PASSWORD_PAGES!}>
       <div className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-black flex flex-col items-center">
-        <div className="mt-8 w-5/6 h-[50rem] ">
+        <div className="mt-8 w-5/6 h-[50rem] mb-8">
           <h1 className="text-4xl font-bold text-white text-center">
             Dashboard Indicadores Mantenimiento General
           </h1>
